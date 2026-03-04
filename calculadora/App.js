6c6434aba077a033f5e465ca27f1d8addc04d048
gallojunior <gallojunior@gmail.com>
2026-03-04 11:43:09 -0300

    terminando o layout

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
@@ -12,8 +12,81 @@ export default function App() {
           </Text>
       </View>
 
+      {/* Linha 1 */}
       <View style={styles.row}>
+        <TouchableOpacity style={styles.functionButton}>
+          <Text style={styles.functionText}>C</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.functionButton}>
+          <Text style={styles.functionText}>+/-</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.functionButton}>
+          <Text style={styles.functionText}>%</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>÷</Text>
+        </TouchableOpacity>
+      </View>
 
+      {/* Linha 2 */}
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>7</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>8</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>9</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>x</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Linha 3 */}
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>4</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>5</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>6</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>-</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Linha 4 */}
+      <View style={styles.row}>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>1</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>2</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>3</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>+</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Linha 5 */}
+      <View style={styles.row}>
+        <TouchableOpacity style={[styles.buttonNumber, styles.doubleWidthButton]}>
+          <Text style={styles.buttonText}>0</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.buttonNumber}>
+          <Text style={styles.buttonText}>.</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.operatorButton}>
+          <Text style={styles.operatorText}>=</Text>
+        </TouchableOpacity>
       </View>
 
     </View>
@@ -42,5 +115,49 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     paddingHorizontal: 10
+  },
+  functionButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height: '75%',
+    backgroundColor: '#A5A5A5',
+  },
+  functionText: {
+    color: '#000',
+    fontSize: 28
+  },
+  operatorButton: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height: '75%',
+    backgroundColor: '#FF9500',
+  },
+  operatorText: {
+    color: '#fff',
+    fontSize: 36
+  },
+  buttonNumber: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginHorizontal: 5,
+    borderRadius: 50,
+    height: '75%',
+    backgroundColor: '#333333',
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 32
+  },
+  doubleWidthButton : {
+    flex: 2,
+    alignItems: 'flex-start',
+    paddingLeft: 32,
   }
 });
